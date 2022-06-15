97d2f3f8d7bd093ba550cb341ac6518bffe89abb
distip <deniz.istipliler@gmail.com>
2022-06-15 17:08:24 -0500

    last changes in r script

--- heritability_code.R
+++ heritability_code.R
@@ -1,18 +1,19 @@
+library(tidyr)
 library(dplyr)
 library(heritability)
-library(tidyr)
+
 data <- read.csv('spectrum.csv')
 
 H <- repeatability(data.vector=data$X750,geno.vector= data$Genotype,
                 covariates.frame=as.data.frame(data[,2]))
 
 columns <- colnames(data %>% dplyr:: select(starts_with('X')))
+
+
 H_all <- c()
-
-for (i in columns) {
-  H <- repeatability(data.vector=data$(i) ,geno.vector= data$Genotype,covariates.frame=as.data.frame(data[,2]))
+for (i in 4:ncol(data)) {
+  H <- repeatability(data.vector=data[,i] ,geno.vector= data$Genotype,covariates.frame=as.data.frame(data[,2]))
   H_all <- c(H_all, H$repeatability)
-  print(H_all)
-}
-
+  }
+print(H_all)
 
